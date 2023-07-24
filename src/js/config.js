dcd95be881fec1edd82ff615191d6cb3a4c53119
emadchy <emadchowdhury.1996@gmail.com>
2023-07-24 18:08:18 +0600

    final commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9c5ecf76-77f5-4670-b400-3f9298d1183c';
+export const MODAL_CLOSE_SEC = 2.5;
